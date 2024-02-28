3f20c9f6916b4f864656b44e84b9ac2acc19ea66
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-02-28 20:19:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	const first1 = s.trim();
+	if(s.length === 0){
+		return "";
+	}
+  let first = first1.split(" ");
+	return first[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+const s = prompt("Enter String:"); 
+alert(firstWord(s)); 
